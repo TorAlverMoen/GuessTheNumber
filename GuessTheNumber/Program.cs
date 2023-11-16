0b11ac0f079c266f0fc5d23f3d442be8979699b7
Tor Alver Moen <tor.alver.moen@gmail.com>
2023-11-16 10:28:36 +0100

    Initial commit

---
+++ GuessTheNumber/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace GuessTheNumber
+{
+    class Program
+    {
+        static void Main()
+        {
+            bool isGameOver = false;
+
+            while (!isGameOver)
+            {
+                Random rnd = new Random();
+                int numToGuess = rnd.Next(1, 101);
+                int playerGuess = 0;
+
+                Console.WriteLine("Guess a number between 1 and 100");
+
+                while (playerGuess != numToGuess)
+                {
+                    Console.Write("Enter your guess: ");
+                    playerGuess = Convert.ToInt32(Console.ReadLine());
+
+                    if (playerGuess < numToGuess)
+                    {
+                        Console.WriteLine("The number is too low");
+                    }
+                    else if (playerGuess > numToGuess)
+                    {
+                        Console.WriteLine("The number is to high");
+                    }
+                }
+
+                Console.WriteLine("You guessed the correct number!");
+            }
+        }
+    }
+}
